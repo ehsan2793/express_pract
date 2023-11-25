5f4c1307dac126fb854afef5c2b820b8ccc1bcf6
ehsan2793 <ehsan2793@gmail.com>
2023-11-24 18:03:36 -0800

    Fix incorrect server URL in console.log

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ app.get("/friends/:friendId", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://${localhost}:${PORT}`);
+  console.log(`Server is running on http://localhost:${localhost}:${PORT}`);
 });
